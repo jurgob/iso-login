e26af0152896a949435db62549027b2683276db7
jurgob <jurgo.boemo@gmail.com>
2015-10-07 10:09:49 +0100

    add redirect on route level

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -6,10 +6,14 @@ import { Route } from 'react-router';
 import LoginForm from './components/LoginPage';
 import HomePage from './components/HomePage';
 
+function requireAuth(nextState, replaceState) {
+  // replaceState({ nextPathname: nextState.location.pathname }, '/login');
+}
+
 let routes = (
   <Route path="/" >
     <Route path="login" component={LoginForm} />
-    <Route path="home" component={HomePage} />
+    <Route path="home" component={HomePage} onEnter={requireAuth} />
   </Route>
 );
 
